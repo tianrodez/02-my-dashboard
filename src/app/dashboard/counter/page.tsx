7df74333d3fbb788e4c3c6c87c78a82b5ca91560
Seb4sC <sacastro081@soy.sena.edu.co>
2025-04-28 15:16:08 -0500

    feat: reintroducir CartCounter y agregar manejo de errores para la página de Pokemons

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,4 +1,4 @@
-import { CartCounter } from "@/app/components/shopping-cart";
+import { CartCounter } from "@/shopping-cart";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
