d3bd3cd10900651cefc3f019ae2906d67c15d052
Seb4sC <sacastro081@soy.sena.edu.co>
2025-04-29 11:17:36 -0500

    - Fixed export default page

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -6,13 +6,11 @@ export const metadata: Metadata = {
   description: "A simple page to manage products in the cart",
 };
 
-export const CounterPage = () => {
+export default function CounterPage() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <span>Productos en el carrito</span>
-      <CartCounter value={20} />      
+      <CartCounter value={20} />
     </div>
   );
-};
-
-export default CounterPage;
+}
